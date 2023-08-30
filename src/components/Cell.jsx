0f499cad27118aeb3e003639fdc407e178a63d86
chelahcynthia <cchelangat989@gmail.com>
2023-08-30 12:31:35 +0300

    Created a function that checks if it's a circle or cross

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function cell({ id, cell, setCells }) {
   const handleClick = (e) => {
+    const taken = e.target.firstChild.classList.contains("circles") || 
+    e.target.firstChild.classList.contains("cross")
    
   };
 
