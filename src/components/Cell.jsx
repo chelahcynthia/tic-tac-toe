69b4fca8cb84830a012ddb7730bc7fa5a201b440
chelahcynthia <cchelangat989@gmail.com>
2023-08-30 13:26:59 +0300

    Added crosses on the squares

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function cell({ id, cell, setCells }) {
   const handleClick = (e) => {
-    const taken = e.target.firstChild.classList.contains("circles") || 
+    const taken = e.target.firstChild.classList.contains("circle") || 
     e.target.firstChild.classList.contains("cross")
    
   };
 
-  return <div className="square" id={id} onClick={handleClick}></div>;
+  return <div className="square" id={id} onClick={handleClick}>
+    <div className="cross"></div>
+  </div>;
 }
